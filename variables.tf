6f13f6871f08e12d31f83e574acce3610355597e
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-09-10 16:50:45 -0400

    iam-account

--- variables.tf
+++ variables.tf
@@ -11,48 +11,48 @@ variable "account_alias" {
 
 variable "hard_expiry" {
     description = "Whether users are prevented from setting a new password after their password has expired (i.e. require administrator reset)"
-    type = boolean
+    type = bool
     default = false
 }
 
 variable "max_password_age" {
     description = "The number of days that an user password is valid"
-    type = integer
+    type = int
     default = 20
 }
 
 variable "minimum_password_length" {
     description = "Minimum length to require for user passwords"
-    type = integer
+    type = int
     default = 8
 }
 
 variable "password_reuse_prevention" {
     description = "The number of previous passwords that users are prevented from reusing"
-    type = integer
+    type = int
     default = 8
 }
 
 variable "require_lowercase_characters" {
     description = "Whether to require lowercase characters for user passwords"
-    type = boolean
+    type = bool
     default = true
 }
 
 variable "require_numbers" {
     description = "Whether to require numbers for user passwords"
-    type = boolean
+    type = bool
     default = true
 }
 
 variable "require_symbols" {
     description = "Whether to require symbols for user passwords"
-    type = boolean
+    type = bool
     default = true
 }
 
 variable "require_uppercase_characters" {
     description = "Whether to require uppercase characters for user passwords"
-    type = boolean
+    type = bool
     default = true
 }
